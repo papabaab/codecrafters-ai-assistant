ff9fed9df57d0f8011b6bf8a6d07b309e4d781e1
Papa Diallo <albaa.app@gmail.com>
2024-06-01 22:55:21 +0000

    feat: added configs of ai assistant to mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ChatsModule } from './chats/chats.module';
+import { AiConfigsModule } from './ai-configs/ai-configs.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      process.env.MONGODB_URL,
+      {
+        dbName: 'ai_assistant',
+      },
+    ),
+    ChatsModule,
+    AiConfigsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,17 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { GoogleGenerativeAI } from '@google/generative-ai';
 import 'dotenv/config';
-import { GENERATION_CONFIG, MODEL_NAME, PARTS, ROLE, SAFETY_SETTINGS, SYSTEM_INSTRUCTIONS } from './configs.ai';
+import { GENERATION_CONFIG, MODEL_NAME, SAFETY_SETTINGS, SYSTEM_INSTRUCTIONS } from './ai-configs/configs.ai';
+import { ChatService } from './chats/chat.service';
+import { AiConfigsService } from './ai-configs/ai-configs.service';
 
 const API_KEY = process.env.GEMINI_API_KEY;
 
 @Injectable()
 export class AppService {
-  constructor() {}
-  getHello(): string {
+  constructor(private chatService: ChatService, private aiConfigs: AiConfigsService) {}
+  async getHello() {
+    // TESTING AI SERVICE
+
+
+    // RUNNING CHAT
     this.run();
-    return 'Hello World!';
+    return 'hello configs test';
   }
+
+
+
   async run() {
     console.log('APP SERVICE: RUN METHOD TO TEST GEMINI ---->');
     const genAI = new GoogleGenerativeAI(API_KEY);
@@ -20,25 +29,13 @@ export class AppService {
       systemInstruction: SYSTEM_INSTRUCTIONS,
     });
 
-    // const result = await model.generateContent({
-    //   contents: [{ role: 'user', parts }],
-    //   generationConfig,
-    //   safetySettings,
-    // });
-
     const chat = model.startChat({
-      history: [
-        {
-          role: ROLE.USER,
-          parts: [PARTS[0]],
-        },
-        {
-          role: ROLE.MODEL,
-          parts: [PARTS[1]],
-        }
-      ], generationConfig: GENERATION_CONFIG,
+      history: await this.chatService.getChatHistory('1765456789'),
+      generationConfig: GENERATION_CONFIG,
       safetySettings: SAFETY_SETTINGS
     });
+
+
 
     const msg = "J'ai un petit boulôt qui me permet d'avoir environ 2000000 XOF par mois. En plus de ça, j'ai des parents riches qui me donnent 300 000 XOF par mois en guise d'argent de poche.";
 
